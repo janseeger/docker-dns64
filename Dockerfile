dae3196935ef6865ff1bc30fe701ddb2f5c8fab9
Amon-Ra <amon.raj@gmail.com>
2018-07-04 00:26:08 +0200

    add listen interfaces

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM debian
 
-ENV DNS64_PREFIX='2a00:1508:6001:efff::/96'
+ENV DNS64_PREFIX='2a00:1508:6001:efff::/96' \
+    DNS64_IP6_LISTEN='any' \
+    DNS64_LISTEN='any'
+
 ADD docker-entry.sh /
 
 RUN apt-get update && apt-get install -y bind9 && apt-get clean && chmod +x docker-entry.sh
--- docker-entry.sh
+++ docker-entry.sh
@@ -4,7 +4,8 @@ cat > /etc/bind/named.conf.options <<EOF
 options {
         directory "/var/cache/bind";
         auth-nxdomain no;
-        listen-on-v6 { any; };
+        listen-on-v6 { ${DNS64_IP6_LISTEN}; };
+        listen-on { ${DNS64_LISTEN}; }
         allow-query { any; };
         dns64 ${DNS64_PREFIX} {
                 clients { any; };
